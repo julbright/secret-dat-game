52f6dd9e1f0455396091060f30ff676c6a7fab5b
Laura <lalorenz6@gmail.com>
2015-03-01 17:27:46 -0500

    Added in detraction from teacher HP when student dies and for student to disappear when their hit points are below 0.

--- characters.js
+++ characters.js
@@ -10,6 +10,10 @@ Quintus.Characters = function(Q) {
       });
       this.add("2d");
       this.add("stepControls");
+    },
+    mourn: function(student){
+      student.destroy();
+      this.p.hitPoints = this.p.hitPoints - 10;
     }
   });
 
@@ -18,14 +22,14 @@ Quintus.Characters = function(Q) {
       this._super(p, {
         type: Q.STUDENT_AND_FURNITURE,
         collisionMask: Q.TEACHER,
-        hitPoints: 500
+        hitPoints: 50
       });
+
       this.add("2d");
       this.on("hit", function(collision){
         if(collision.obj.isA("Teacher")){
           console.log("Thanks Aaron!");
           this.p.hitPoints = this.p.hitPoints + 1;
-          console.log(this.p.hitPoints);
         }
         else{
           console.log(collision.obj);
@@ -33,7 +37,13 @@ Quintus.Characters = function(Q) {
       });
     },
     step: function(dt){
-      this.p.hitPoints = this.p.hitPoints - 0.0001;
+      if(this.p.hitPoints > 0){
+      this.p.hitPoints = this.p.hitPoints - 1;
+      console.log(this.p.hitPoints);
+    }
+      else{
+        this.trigger("death", this);
+      }
     }
   });
 }
