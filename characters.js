c742cd0a8bfd6401f13d50a875bc6683f9df846c
Laura <lalorenz6@gmail.com>
2015-03-01 16:55:28 -0500

    Adding in hit point calculations for student boredom and for teacher helping

--- characters.js
+++ characters.js
@@ -5,6 +5,7 @@ Quintus.Characters = function(Q) {
         type: Q.TEACHER,
         x: 150,
         y: 60,
+        hitPoints: 100,
         collisionMask: Q.STUDENT_AND_FURNITURE
       });
       this.add("2d");
@@ -16,17 +17,23 @@ Quintus.Characters = function(Q) {
     init: function(p) {
       this._super(p, {
         type: Q.STUDENT_AND_FURNITURE,
-        collisionMask: Q.TEACHER  
+        collisionMask: Q.TEACHER,
+        hitPoints: 500
       });
       this.add("2d");
       this.on("hit", function(collision){
         if(collision.obj.isA("Teacher")){
           console.log("Thanks Aaron!");
+          this.p.hitPoints = this.p.hitPoints + 1;
+          console.log(this.p.hitPoints);
         }
         else{
           console.log(collision.obj);
         }
       });
+    },
+    step: function(dt){
+      this.p.hitPoints = this.p.hitPoints - 0.0001;
     }
   });
 }
