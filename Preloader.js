86f5e696618a64893aaeb6a0d6db3695f6637e2a
lauralorenz <lalorenz6@gmail.com>
2015-06-28 10:39:57 -0400

    Lots of updates to the refactor! Working on some global catching. Also, ignoring DS_Store files.

--- Preloader.js
+++ Preloader.js
@@ -23,10 +23,10 @@ BasicGame.Preloader.prototype = {
 		//this.load.setPreloadSprite(this.preloadBar);
 
 		//	Here we load the rest of the assets our game needs.
-	    game.load.tilemap('map', 'data/background_layer.csv', null, Phaser.Tilemap.CSV);
-	    game.load.image('tiles', "images/tiles.png");
-	    game.load.spritesheet('dude_tiles', 'images/tiles.png', 32, 32);
-	    game.load.spritesheet('aaron', 'images/townfolk1_m.png', 32, 36, 12)
+	    this.load.tilemap('map', 'data/background_layer.csv', null, Phaser.Tilemap.CSV);
+	    this.load.image('tiles', "images/tiles.png");
+	    this.load.spritesheet('dude_tiles', 'images/tiles.png', 32, 32);
+	    this.load.spritesheet('aaron', 'images/townfolk1_m.png', 32, 36, 12)
 
 
 	},
@@ -37,25 +37,6 @@ BasicGame.Preloader.prototype = {
 		//this.preloadBar.cropEnabled = false;
 
 		this.state.start('MainMenu');
-
-	},
-
-	update: function () {
-
-		//	You don't actually need to do this, but I find it gives a much smoother game experience.
-		//	Basically it will wait for our audio file to be decoded before proceeding to the MainMenu.
-		//	You can jump right into the menu if you want and still play the music, but you'll have a few
-		//	seconds of delay while the mp3 decodes - so if you need your music to be in-sync with your menu
-		//	it's best to wait for it to decode here first, then carry on.
-		
-		//	If you don't have any music in your game then put the game.state.start line into the create function and delete
-		//	the update function completely.
-		
-		if (this.cache.isSoundDecoded('titleMusic') && this.ready == false)
-		{
-			this.ready = true;
-			this.state.start('MainMenu');
-		}
 
 	}
 
--- game.js
+++ game.js
@@ -1,35 +1,73 @@
 
-var height = 1080;
-var width = 1920;
-var game = new Phaser.Game(width, height, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+BasicGame.Game = function (game) {
+
+    //  When a State is added to Phaser it automatically has the following properties set on it, even if they already exist:
+
+    this.game;      //  a reference to the currently running game (Phaser.Game)
+    this.add;       //  used to add sprites, text, groups, etc (Phaser.GameObjectFactory)
+    this.camera;    //  a reference to the game camera (Phaser.Camera)
+    this.cache;     //  the game cache (Phaser.Cache)
+    this.input;     //  the global input manager. You can access this.input.keyboard, this.input.mouse, as well from it. (Phaser.Input)
+    this.load;      //  for preloading assets (Phaser.Loader)
+    this.math;      //  lots of useful common math operations (Phaser.Math)
+    this.sound;     //  the sound manager - add a sound, play one, set-up markers, etc (Phaser.SoundManager)
+    this.stage;     //  the game stage (Phaser.Stage)
+    this.time;      //  the clock (Phaser.Time)
+    this.tweens;    //  the tween manager (Phaser.TweenManager)
+    this.state;     //  the state manager (Phaser.StateManager)
+    this.world;     //  the game world (Phaser.World)
+    this.particles; //  the particle manager (Phaser.Particles)
+    this.physics;   //  the physics manager (Phaser.Physics)
+    this.rnd;       //  the repeatable random number generator (Phaser.RandomDataGenerator)
+
+    //  You can use any of these from any function within this State.
+    //  But do consider them as being 'reserved words', i.e. don't create a property for your own game called "world" or you'll over-write the world reference.
+
+    this.height = 1080;
+    this.width = 1920;
 
 
-var cursors;
-var player;
-var cursor;
+    this.cursors;
+    this.player;
+    this.cursor;
 
-var counter = 60;
-var text = 0;
+    this.counter = 60;
+    this.text = 0;
 
-var timer;
+    this.timer;
+};
 
-function updateCounter() {
-
-    counter--;
-
-    text.setText('Counter: ' + counter);
-
-}
+BasicGame.Game.prototype = {
 
 
-function render() {
+quitGame: function (pointer) {
 
-    game.debug.text("Time until event: " + game.time.events.duration.toFixed(0), 32, 32);
-    game.debug.text("Next tick: " + game.time.events.next.toFixed(0), 32, 64);
+    //  Here you should destroy anything you no longer need.
+    //  Stop music, delete sprites, purge caches, free resources, all that good stuff.
 
-}
+    //  Then let's go back to the main menu.
+    this.state.start('MainMenu');
 
-function preload() {
+},
+
+
+updateCounter: function () {
+
+    this.counter--;
+
+    text.setText('Counter: ' + this.counter);
+
+},
+
+
+render: function() {
+
+    this.game.debug.text("Time until event: " + this.game.time.events.duration.toFixed(0), 32, 32);
+    this.game.debug.text("Next tick: " + this.game.time.events.next.toFixed(0), 32, 64);
+
+},
+
+preload: function () {
 
   //game.load.image('sky', 'assets/sky.png');
    // game.load.image('ground', 'assets/platform.png');
@@ -37,147 +75,150 @@ function preload() {
     //game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 
 
-    game.load.tilemap('map', 'data/background_layer.csv', null, Phaser.Tilemap.CSV);
-    game.load.image('tiles', "images/tiles.png");
-    game.load.spritesheet('dude_tiles', 'images/tiles.png', 32, 32);
-    game.load.spritesheet('aaron', 'images/townfolk1_m.png', 32, 36, 12)
-}
+    this.game.load.tilemap('map', 'data/background_layer.csv', null, Phaser.Tilemap.CSV);
+    this.game.load.image('tiles', "images/tiles.png");
+    this.game.load.spritesheet('dude_tiles', 'images/tiles.png', 32, 32);
+    this.game.load.spritesheet('aaron', 'images/townfolk1_m.png', 32, 36, 12)
+},
 
 //spritesheet(key, url, frameWidth, frameHeight, frameMax, margin, spacing) → {Phaser.Loader}
 
-function create() {
+ createMenu: function() {
+        // When the pause button is pressed, we pause the game
+        this.game.paused = true;
 
-  game.physics.startSystem(Phaser.Physics.ARCADE);
+        // Then add the menu
+        menu = this.add.sprite(this.width/2, this.height/2, 'tiles');
+        menu.anchor.setTo(0.5, 0.5);
+
+        // And a label to illustrate which menu item was chosen. (game is not necessary)
+        this.choiceLabel = this.add.text(this.width/2, this.height-150, 'Click outside menu to continue', { font: '30px Arial', fill: '#fff' });
+        this.choiceLabel.anchor.setTo(0.5, 0.5);
+    },
+
+   
+
+// And finally the method that handles the pause menu
+ unpause: function(event){
+    // Only act if paused
+    if(this.game.paused){
+        // Calculate the corners of the menu
+        var x1 = this.width/2 - 270/2, x2 = this.width/2 + 270/2,
+            y1 = this.height/2 - 180/2, y2 = this.height/2 + 180/2;
+
+        // Check if the click was inside the menu
+        if(event.x > x1 && event.x < x2 && event.y > y1 && event.y < y2 ){
+            // The choicemap is an array that will help us see which item was clicked
+            var choisemap = ['one', 'two', 'three', 'four', 'five', 'six'];
+
+            // Get menu local coordinates for the click
+            var x = event.x - x1,
+                y = event.y - y1;
+
+            // Calculate the choice 
+            var choise = Math.floor(x / 90) + 3*Math.floor(y / 90);
+
+            // Display the choice
+            this.choiceLabel.text = 'You chose menu item: ' + choisemap[choise];
+        }
+        else{
+            // Remove the menu and the label
+            menu.destroy();
+            choiceLabel.destroy();
+
+            // Unpause the game
+            this.game.paused = false;
+        }
+    }
+},
+
+create: function () {
+
+  this.physics.startSystem(Phaser.Physics.ARCADE);
 
   //create the sprites and tiles
 
-  var map = game.add.tilemap('map', 32, 32);
+  var map = this.add.tilemap('map', 32, 32);
   map.addTilesetImage('tiles');
 
   var layer = map.createLayer(0);
   layer.resizeWorld(); //TODO - what is game
 
-  chairs = game.add.group();
+  chairs = this.add.group();
   chairs.enableBody = true;
   chairs.physicsBodyType = Phaser.Physics.ARCADE;
 
   var chair = chairs.create(32,64, 'dude_tiles', 2);
   chair.body.immovable = true;
   chair.lifespan = 10000;
-  chair.time_taught = counter //TODO: Time taught shouldn't start at 60?
+  chair.time_taught = this.counter //TODO: Time taught shouldn't start at 60?
 
     /*
         Code for the pause menu
     */
 
     // Create a label to use as a button
-    pause_label = game.add.text(width-100, 20, 'Pause', { font: '24px Arial', fill: '#000' });
+    pause_label = this.add.text(this.width-100, 20, 'Pause', { font: '24px Arial', fill: '#000' });
     pause_label.inputEnabled = true;
     pause_label.events.onInputUp.add(createMenu, self);
 
-    function createMenu() {
-        // When the pause button is pressed, we pause the game
-        game.paused = true;
+     // Add a input listener that can help us return from being paused
+    this.game.input.onDown.add(this.unpause, self);
 
-        // Then add the menu
-        menu = game.add.sprite(width/2, height/2, 'tiles');
-        menu.anchor.setTo(0.5, 0.5);
-
-        // And a label to illustrate which menu item was chosen. (game is not necessary)
-        choiceLabel = game.add.text(width/2, height-150, 'Click outside menu to continue', { font: '30px Arial', fill: '#fff' });
-        choiceLabel.anchor.setTo(0.5, 0.5);
-    }
-
-    // Add a input listener that can help us return from being paused
-    game.input.onDown.add(unpause, self);
-
-    // And finally the method that handles the pause menu
-    function unpause(event){
-        // Only act if paused
-        if(game.paused){
-            // Calculate the corners of the menu
-            var x1 = width/2 - 270/2, x2 = width/2 + 270/2,
-                y1 = height/2 - 180/2, y2 = height/2 + 180/2;
-
-            // Check if the click was inside the menu
-            if(event.x > x1 && event.x < x2 && event.y > y1 && event.y < y2 ){
-                // The choicemap is an array that will help us see which item was clicked
-                var choisemap = ['one', 'two', 'three', 'four', 'five', 'six'];
-
-                // Get menu local coordinates for the click
-                var x = event.x - x1,
-                    y = event.y - y1;
-
-                // Calculate the choice 
-                var choise = Math.floor(x / 90) + 3*Math.floor(y / 90);
-
-                // Display the choice
-                choiceLabel.text = 'You chose menu item: ' + choisemap[choise];
-            }
-            else{
-                // Remove the menu and the label
-                menu.destroy();
-                choiceLabel.destroy();
-
-                // Unpause the game
-                game.paused = false;
-            }
-        }
-    };
-
-  text = game.add.text(game.world.centerX, 500, 'Counter: 60', { font: "24px Arial", fill: "#ffffff", align: "center" });
+  text = this.add.text(this.world.centerX, 500, 'Counter: 60', { font: "24px Arial", fill: "#ffffff", align: "center" });
   text.anchor.setTo(0.5, 0.5);
 
   //create the timer
-  timer = game.time.create(autodestroy=false);
-  timer.loop(Phaser.Timer.SECOND, updateCounter, game);
-  timer.add(2000, endRound, game, createMenu)
+  timer = this.time.create(autodestroy=false);
+  timer.loop(Phaser.Timer.SECOND, this.updateCounter, this.game);
+  timer.add(2000, this.endRound, this.game, this.createMenu)
   timer.start()
 
-  //create player
-  player = game.add.sprite(64, 96, 'aaron', 7);
+  //createthis.player
+  this.player = this.add.sprite(64, 96, 'aaron', 7);
 
-  game.physics.arcade.enable(player);
-  player.body.gravity.y = 0;
-  player.body.collideWorldBounds = true;
+  this.physics.arcade.enable(this.player);
+  this.player.body.gravity.y = 0;
+  this.player.body.collideWorldBounds = true;
 
-  player.health = 10000;
+  this.player.health = 10000;
 
-  player.animations.add('left', [9, 10, 11], 10, true);
-  player.animations.add('right', [3,4,5], 10, true);
-  player.animations.add('up', [0,1,2], 10, true);
-  player.animations.add('down', [6,7,8], 10, true);
+  this.player.animations.add('left', [9, 10, 11], 10, true);
+  this.player.animations.add('right', [3,4,5], 10, true);
+  this.player.animations.add('up', [0,1,2], 10, true);
+  this.player.animations.add('down', [6,7,8], 10, true);
 
   
 
-  cursors = game.input.keyboard.createCursorKeys();
+  cursors = this.game.input.keyboard.createCursorKeys();
 
   chair.events.onKilled.add(function(chair){
     console.log(this);
     console.log(this.health);
     this.damage(10);
     console.log(this.health);
-  },player);
-}
-function endRound(createMenu){
+  },this.player);
+},
+
+endRound: function(createMenu){
   console.log("suppppp");
   createMenu();
-};
+},
 
 // function mourn(player){
 //     console.log(player);
 //     console.log(player.health);
-//     player.damage(10);
+//    this.player.damage(10);
 //     console.log(player.health);
 //   };
 
-function update() {
+update: function() {
 
   function collideCallback(player, chair){
     // lose points
     if (check_for_teach(player, chair)){
         chair.lifespan = chair.lifespan + 100;
-        chair.time_taught = counter;
+        chair.time_taught = this.counter;
       }
   };
 
@@ -190,7 +231,7 @@ function update() {
     // and 2 seconds have passed and the counter is now 48
     // 50 > 53 is false, return false
 
-    if (chair.time_taught > counter + 5){
+    if (chair.time_taught > this.counter + 5){
       return true
     }
     else {
@@ -199,64 +240,66 @@ function update() {
   };
  //collision masks
 
-  game.physics.arcade.collide(player, chairs, collideCallback);
+  this.physics.arcade.collide(this.player, chairs, collideCallback);
   
  // movement controls
   if (cursors.left.isDown)
     {
         //  Move to the left
-        player.body.velocity.x = -150;
+       this.player.body.velocity.x = -150;
 
-        player.animations.play('left');
+       this.player.animations.play('left');
 
-        cursor = "left";
+        this.cursor = "left";
     }
     else if (cursors.right.isDown)
     {
         //  Move to the right
-        player.body.velocity.x = 150;
+       this.player.body.velocity.x = 150;
 
-        player.animations.play('right');
+       this.player.animations.play('right');
 
-        cursor = "right";
+        this.cursor = "right";
     }
     else if (cursors.down.isDown)
     {
         //  Move to the right
-        player.body.velocity.y = 150;
+       this.player.body.velocity.y = 150;
 
-        player.animations.play('down');
+       this.player.animations.play('down');
 
-        cursor = "down";
+        this.cursor = "down";
     }
     else if (cursors.up.isDown)
     {
         //  Move to the right
-        player.body.velocity.y = -150;
+       this.player.body.velocity.y = -150;
 
-        player.animations.play('up');
+       this.player.animations.play('up');
 
-        cursor = "up";
+        this.cursor = "up";
     }
     else
     {
         //  Stand still
-        player.animations.stop();
+       this.player.animations.stop();
 
-        player.body.velocity.x = 0;
-        player.body.velocity.y = 0;
+       this.player.body.velocity.x = 0;
+       this.player.body.velocity.y = 0;
         
-        if (cursor == "up"){
-          player.frame = 1;
+        if (this.cursor == "up"){
+         this.player.frame = 1;
         }
-        else if (cursor == "left"){
-          player.frame = 10;
+        else if (this.cursor == "left"){
+         this.player.frame = 10;
         }
-        else if (cursor == "right"){
-          player.frame = 4;
+        else if (this.cursor == "right"){
+         this.player.frame = 4;
         }
         else {
-          player.frame = 7;
+         this.player.frame = 7;
         } 
     }
 }
+
+};
